727bd8b440cd5e48df0240e305aca9c867690bd2
unknown <nico.volkens@mailbox.tu-dresden.de>
2024-02-18 21:41:12 +0100

    dropdown can now handle click outside

--- frontend/src/components/UnitsDropdown.vue
+++ frontend/src/components/UnitsDropdown.vue
@@ -1,11 +1,12 @@
 <script setup>
-import {ref, computed, defineProps, defineEmits, defineModel, onMounted, onUnmounted} from 'vue'
-
+import {ref, computed, defineProps, defineEmits, onMounted} from 'vue'
 
 const props = defineProps(["unitList","modelValue"])
 const emit = defineEmits(["update:modelValue"])
 
+const dropdownRoot = ref(null)
 const isOpen = ref(false)
+
 
 const filteredUnitList = computed(() => {
   let newFilteredList = []
@@ -17,12 +18,20 @@ const filteredUnitList = computed(() => {
     return newFilteredList 
 })
 
+function closeWhenInside(e){	
+    if (!dropdownRoot.value.contains(e.target)){
+        closeDropdown();
+    }
+}
+
 function closeDropdown(){
     isOpen.value = false
+    window.removeEventListener('focusin', closeWhenInside)
 }
 
 function openDropdown(){
     isOpen.value = true
+    window.addEventListener('focusin', closeWhenInside)
 }
 
 function selectItem(index){
@@ -33,7 +42,7 @@ function selectItem(index){
 </script>
 
 <template>
-    <div @focusin="openDropdown">
+    <div ref="dropdownRoot" @focusin="openDropdown">
         <input type="text" :value="props.modelValue" @input="emit('update:modelValue', $event.target.value)" placeholder="Einheit">
         <div v-show="isOpen">
             <a href="#" v-for="unit,i in filteredUnitList" :key="i" @click="selectItem(i)">
